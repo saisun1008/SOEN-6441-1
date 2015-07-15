36b6df268c8063598435399066a8a0fcf52a7ed0
ryan-yin <ryan.yin@gmail.com>
2015-07-14 23:52:41 -0700

    add test in testboardbuilder

--- SOEN-6441/src/test/hf/game/TestBoardBuilder.java
+++ SOEN-6441/src/test/hf/game/TestBoardBuilder.java
@@ -21,12 +21,45 @@ public class TestBoardBuilder
         builder = new GameBoardBuildedr(board);
     }
 
+
     @Test
     public void testLoadLakeTile()
     {
+        assertTrue(board.getLakeTileCollection().size() == 0);
+        // function to be tested
         builder.loadLakeTileCard();
         assertTrue("Lake tile load failed", board.getLakeTileCollection()
                 .size() == CardType.LAKETILE.getNumberOfCard());
     }
 
+    @Test
+    public void testLoadLanternCard()
+    {
+        assertTrue(board.getLatternCollection().size() == 0);
+        builder.loadLatternCard();
+        assertTrue(board.getLatternCollection().size() == CardType.LATERN
+                .getNumberOfCard());
+    }
+
+    @Test
+    public void testloadDedicationCard()
+    {
+        assertTrue(board.getDedicationTokenCollection().size() == 0);
+        builder.loadDedicationCard();
+        assertTrue("actual number is "
+                + board.getDedicationTokenCollection().size(), board
+                .getDedicationTokenCollection().size() == CardType.DEDICATION
+                .getNumberOfCard());
+    }
+
+    @Test
+    public void testloadFavorToken()
+    {
+        assertTrue(board.getFavorTokenCollection().size() == 0);
+        builder.loadFavorToken();
+        assertTrue(
+                "actual number is " + board.getFavorTokenCollection().size(),
+                board.getFavorTokenCollection().size() == CardType.FAVOR
+                        .getNumberOfCard());
+    }
 }
